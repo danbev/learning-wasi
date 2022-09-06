21069c4fe1f5a541d85de3350845d2c407ca4fa9
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-09-06 06:26:13 +0200

    Add logging of imports and exports for tee module

--- src/tee.js
+++ src/tee.js
@@ -6,5 +6,9 @@ const buffer = fs.readFileSync('out/tee.wasm');
 WebAssembly.validate(buffer);
 
 WebAssembly.instantiate(buffer, {console}).then((results) => {
+  const imports = WebAssembly.Module.imports(results.module);
+  console.log('imports: ', imports);
+  const exports = WebAssembly.Module.exports(results.module);
+  console.log('exports: ', exports);
   results.instance.exports.main();
 });
