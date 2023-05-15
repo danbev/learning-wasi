f12130b97948c29558162de3ceae78ea6b4b72e4
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-05-15 12:38:19 +0200

    Remove wildcard import of modules
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- wasmtime-example/wasmtime/src/wasm.rs
+++ wasmtime-example/wasmtime/src/wasm.rs
@@ -1,13 +1,6 @@
-/*
-use anyhow::Result;
-use wasmtime::component::{bindgen, Component as WasmComponent, Linker};
-use wasmtime::{Config, Engine, Store};
-use wasmtime_wasi::WasiCtxBuilder;
-*/
-
 bindgen!(in "../wasm-component/wit");
 
-use wasmtime::component::*;
+use wasmtime::component::{bindgen, Component, Linker};
 use wasmtime::{Config, Engine, Store};
 
 struct MyState {
@@ -24,7 +17,9 @@ fn main() -> wasmtime::Result<()> {
     let mut config = Config::new();
     config.wasm_component_model(true);
     let engine = Engine::new(&config)?;
+
     let component = Component::from_file(&engine, "../wasm-component/example-wasm-component.wasm")?;
+
     let mut linker = Linker::new(&engine);
     Wasmcomponent::add_to_linker(&mut linker, |state: &mut MyState| state)?;
     let mut store = Store::new(
@@ -33,7 +28,7 @@ fn main() -> wasmtime::Result<()> {
             name: "me".to_string(),
         },
     );
-    let (bindings, _) = Wasmcomponent::instantiate(&mut store, &component, &linker)?;
+    let (bindings, _instance) = Wasmcomponent::instantiate(&mut store, &component, &linker)?;
 
     let ret = bindings.call_something(&mut store, "bajja")?;
     println!("ret: {ret:?}");
