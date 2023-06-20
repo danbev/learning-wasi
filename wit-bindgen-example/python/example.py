9668bed69ead19c63b224a83b5826b46f49f705c
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-06-20 10:04:40 +0200

    Update python example to be able to run
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- wit-bindgen-example/python/example.py
+++ wit-bindgen-example/python/example.py
@@ -1,9 +1,9 @@
-from dist import ExampleComponent
+from dist import Root, RootImports
 from wasmtime import Store
 
 def main():
     store = Store()
-    component = ExampleComponent(store)
+    component = Root(store, RootImports(None, None, None, None, None, None, None, None))
     ret = component.something(store, "bajja")
     print(ret)
 
