07245eb2fb36f85480553aacc4bf0cd3760eed42
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-07-06 09:01:48 +0200

    Cleanup args code
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- compose-example/static-eval/src/lib.rs
+++ compose-example/static-eval/src/lib.rs
@@ -14,18 +14,16 @@ impl StaticEvaluator for Export {
         let bytes = include_bytes!("../policy.dog");
         let policy = str::from_utf8(bytes).unwrap();
 
-        let input = env::args().nth(1);
-        if input.is_none() {
+        let Some(input) = std::env::args().nth(1) else {
             return "Error: input to evaluate must be specified as an argument".to_string();
-        }
+        };
 
-        if let Ok(policy_name) = env::var("SEEDWING_POLICY_NAME") {
-            println!("policy_name: {policy_name}");
-            println!("input: {}", input.unwrap());
-            return engine::eval(policy);
-        } else {
+        let Ok(policy_name) = env::var("SEEDWING_POLICY_NAME") else {
             return "Error: Policy name must be set!".to_string();
-        }
+        };
+        println!("policy_name: {policy_name}");
+        println!("input: {}", input);
+        return engine::eval(policy);
     }
 }
 export_static_evaluator!(Export);
